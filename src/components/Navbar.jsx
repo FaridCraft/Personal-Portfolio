2052dc670fd29005f0e236a5dc0d13c92e58933d
Farid Ullah <fareed123456yt@gmail.com>
2026-05-11 11:36:06 -0700

    Updated portfolio design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -59,7 +59,7 @@ export function Navbar() {
                 onClick={() => setIsMobileMenuOpen(false)}
               >
                 {link}
-                <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-gradient-to-r from-cyan-400 to-purple-500 group-hover:w-full transition-all duration-300" />
+                <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-linear-to-r from-cyan-400 to-purple-500 group-hover:w-full transition-all duration-300" />
               </motion.a>
             ))}
             {/* Removed: <ThemeToggle isDark={isDark} toggleTheme={toggleTheme} /> */}
